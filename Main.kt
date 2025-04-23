1c519f71d9943551f2633f4ae4bf178cf41c2d38
Akritiku <140236651+Akritiku@users.noreply.github.com>
2025-04-23 16:07:27 +0530

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,10 @@
+package com.example.weather_app
+
+data class Main(
+    val temp: Double,
+    val pressure: Int,
+    val humidity: Int,
+    val temp_max: Double,
+    val temp_min: Double,
+    val sea_level: Int
+)
---
+++ Weather.kt
@@ -0,0 +1,12 @@
+package com.example.weather_app
+
+data class Weather(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
+
+//class weather {
+//
+//}
